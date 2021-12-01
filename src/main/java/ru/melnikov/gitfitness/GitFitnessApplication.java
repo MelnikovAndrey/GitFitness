5cec27dbfcc37f2ff61aee630a589ea613b086e4
MelnikovAndrey <Ljkujcnhjq99>
2021-12-01 18:46:04 +0300

    Initial commit

---
+++ src/main/java/ru/melnikov/gitfitness/GitFitnessApplication.java
@@ -0,0 +1,13 @@
+package ru.melnikov.gitfitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitFitnessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitFitnessApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/melnikov/gitfitness/GitFitnessApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.melnikov.gitfitness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitFitnessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
